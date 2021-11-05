d1c9aef0a907e156364a7a51910a64a01e108927
UROS <nonamen212@gmail.com>
2021-11-05 22:31:56 +0100

    Changes #2

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,43 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Main.css";
 import { articles } from "../util/articles.js";
 
 const Main = () => {
+  const [object, setObject] = useState({
+    id: "",
+    brand: "",
+    name: "",
+    sizes: "",
+    price: "",
+    discount: "",
+    colour: "",
+    picture: "",
+  });
+  const [visible,setVisible] = useState("modal-wrapper-hidden")
+
+  const openModal = (passedId) => {
+    articles.forEach((article) =>
+      article.id === passedId
+        ? setObject((state) => ({
+            ...state,
+            id: article.id,
+            brand: article.brand,
+            name: article.name,
+            sizes: article.sizes,
+            price: article.price,
+            discount: article.discount,
+            colour: article.colour,
+            picture: article.picture,
+          }))
+        : null
+    );
+    setVisible("modal-wrapper")
+  };
+
   return (
     <div className="wrapper">
       <div className="main">
         {articles.map((data, index) => {
           return (
-            <div id={data.id}>
+            <div className="article-item" id={data.id} key={data.id} onClick={() => openModal(data.id)}>
               <img src={data.picture} alt={data.name} />
               <div className="brand-container">
-              <p className="brand">{data.brand}</p>
+                <p className="brand">{data.brand}</p>
               </div>
               <div className="name-container">
-              <p className="name">{data.name}</p>
+                <p className="name">{data.name}</p>
               </div>
               <div className="price-container">
-                {
-                    !data.discount ? (
-                        <p className="price">{data.price} RSD</p>
-                    ) : (
-                        <p className="price slashed">{data.price} RSD</p>
-                    )
-                }
-                {
-                    !data.discount ? (
-                        <span></span>
-                    ) : (
-                        <p className="discount">{data.discount} RSD</p>
-                    )
-                }
+                {!data.discount ? (
+                  <p className="price">{data.price} RSD</p>
+                ) : (
+                  <p className="price slashed">{data.price} RSD</p>
+                )}
+                {!data.discount ? (
+                  <span></span>
+                ) : (
+                  <p className="discount">{data.discount} RSD</p>
+                )}
               </div>
             </div>
           );
         })}
-      </div>
-      <div className="modal">
-          
+        <span className={`${visible}`}>
+          <div className="modal">
+            <div>
+              <img src={object.picture} alt={object.name} />
+            </div>
+            <div>
+                <p className="modal-brand">{object.brand}</p>
+                <p className="modal-name">{object.name}</p>
+            </div>
+          </div>
+        </span>
       </div>
     </div>
   );
