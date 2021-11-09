8c7068daa82360251d1d6cb835bc5d932d905ef5
UROS <nonamen212@gmail.com>
2021-11-09 11:37:57 +0100

    Changes #9

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React from 'react'
-import './App.css';
-import AppNavbar from './components/AppNavbar.jsx'
-import Main from './components/Main.jsx'
-import AppFooter from './components/AppFooter.jsx'
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import AppNavbar from "./components/AppNavbar.jsx";
+import Main from "./components/Main.jsx";
+import AppFooter from "./components/AppFooter.jsx";
 
 const App = () => {
-  return (
-    <div>
-      <AppNavbar/>
-      <Main/>
-      <AppFooter/>
-    </div>
-  )
-}
+	const [searchValue, setSearchValue] = useState("");
 
-export default App
+	const itemsInCart = JSON.parse(localStorage.getItem("cart")) || [];
+	const [cart, setCart] = useState(itemsInCart);
+	const openCart = useState();
+	const closeCart = useState();
+
+	useEffect(() => {
+		localStorage.setItem("cart", JSON.stringify(cart));
+	}, [cart]);
+
+	useEffect(() => {
+		setCart(JSON.parse(localStorage.getItem("cart")));
+		// console.log(JSON.parse(localStorage.getItem("cart")));
+	}, []);
+
+	return (
+		<div>
+			<AppNavbar searchValue={searchValue} setSearchValue={setSearchValue} openCart={openCart} closeCart={closeCart} cart={cart} />
+			<Main searchValue={searchValue} setCart={setCart} cart={cart} />
+			<AppFooter />
+		</div>
+	);
+};
+
+export default App;
--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -2,32 +2,70 @@ import React from "react";
 import "./AppNavbar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faShoppingCart, faSearch,faBiohazard } from "@fortawesome/free-solid-svg-icons";
+import { faShoppingCart, faSearch, faBiohazard } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react/cjs/react.development";
 
-library.add(faShoppingCart,faSearch,faBiohazard);
+library.add(faShoppingCart, faSearch, faBiohazard);
 
-const AppNavbar = () => {
-  return (
-    <div className="nav-container">
-        <div className="nav-title-small">
-        <FontAwesomeIcon icon="biohazard" />
-        </div>
-      <div className="nav-title">
-        <p>SNEAKERS</p>
-      <div className="nav-search">
-        <input type="text" className="search-input" placeholder="Search"/>
-        <FontAwesomeIcon icon="search" />
-      </div>
-      </div>
-      <div className="nav-search-small">
-        <input type="text" className="search-input" placeholder="Search"/>
-        <FontAwesomeIcon icon="search" />
-      </div>
-      <div className="nav-shopping-cart">
-        <FontAwesomeIcon icon="shopping-cart" />
-      </div>
-    </div>
-  );
+const AppNavbar = ({ searchValue, setSearchValue, cart }) => {
+	const [visible, setVisible] = useState("modal-wrapper-hidden");
+
+	const openCart = () => {
+		console.log("OPEN CART");
+		setVisible("modal-wrapper");
+	};
+
+	const closeCart = () => {
+		setVisible("modal-wrapper-hidden");
+	};
+	console.log(cart);
+	return (
+		<div className="nav-container">
+			<div className="nav-title-small">
+				<FontAwesomeIcon icon="biohazard" />
+			</div>
+			<div className="nav-title">
+				<p>SNEAKERS</p>
+				<div className="nav-search">
+					<input type="text" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} className="search-input" placeholder="Search" />
+					<FontAwesomeIcon icon="search" />
+				</div>
+			</div>
+			<div className="nav-search-small">
+				<input type="text" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} className="search-input" placeholder="Search" />
+				<FontAwesomeIcon icon="search" />
+			</div>
+			<div className="nav-shopping-cart" onClick={openCart}>
+				<FontAwesomeIcon icon="shopping-cart" />
+			</div>
+			<span className={`${visible}`}>
+				<div className="modal add-to-cart">
+					{cart.map((item) => {
+						console.log(item.idItemCart);
+						return (
+							<div className="article-item" id={item.article.id} key={item.idItemCart}>
+								<img src={item.article.picture} alt={item.article.name} />
+								<div className="brand-container">
+									<p className="brand">{item.article.brand}</p>
+								</div>
+								<div className="name-container">
+									<p className="name">{item.article.name}</p>
+								</div>
+								<div className="price-container">
+									<p>{item.totalPrice}</p>
+								</div>
+								<div className="size-container">
+									<p>{item.size}</p>
+								</div>
+								<input type="number" value={item.quantity} readOnly />
+							</div>
+						);
+					})}
+					<FontAwesomeIcon icon="times" onClick={closeCart} className="close-modal" />
+				</div>
+			</span>
+		</div>
+	);
 };
 
 export default AppNavbar;
